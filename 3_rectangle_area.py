e9df5172c0377602acf02ef9a7f38899d9f75d79
kyle williams <72155617+ky13-troj@users.noreply.github.com>
2021-05-29 21:36:15 +0530

    Create 3_rectangle_area.py

---
+++ 3_rectangle_area.py
@@ -0,0 +1,6 @@
+x1 = int(input("Enter x co ordinate of 1st vertex:"))
+y1 = int(input("Enter y co ordinate of 1st vertex:"))
+x2 = int(input("Enter x co ordinate of 2nd vertex:"))
+y2 = int(input("Enter y co ordinate of 2nd vertex:"))
+area = (x2 - x1)*(y2-y1)
+print("Area: ",area)
