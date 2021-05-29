d6033ee64b38d384591330a3d329a83c6d49d437
kyle williams <72155617+ky13-troj@users.noreply.github.com>
2021-05-29 21:34:07 +0530

    Create AP.py

---
+++ AP.py
@@ -0,0 +1,6 @@
+ip = int(input("Enter 1st term:"))
+ip_2 = int(input("Enter 2nd term:"))
+ip_3 = int(input("Enter 3rd term:"))
+if ip_2 - ip == ip_3 - ip_2 :
+  d = ip_2 - ip
+print("Common Diff:",d)
